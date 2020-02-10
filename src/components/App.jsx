1123d2dca8fca88e38f7ba799d95d8bbcef8fe1c
Adilet Momunaliev and Joseph Tubridy <adilet@>
2020-02-10 15:39:20 -0800

    add html to outine how it will go together

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,52 @@
 import React from "react";
 //import TicketList from "./TicketList";
 import Header from "./Header";
+inport Background from "./Background";
 
 function App(){
   return (
     <div>
       <Header/>
+      <Background>
+        <div>
+          <TopRow>
+            <div>
+              <Home/>
+              <Notifications/>
+              <Messages/>
+              <SearchInput/>
+              <Tweet/>
+            </div>
+          </TopRow>
+          <Mainperson>
+            <Picture/>
+            <Name/>
+            <Tweets/>
+            <Following/>
+            <Followers/>
+          </Mainperson>
+          <WhatTheySaidBox/>
+          <FeedBox>
+            <PostRow>
+              <Picture/>
+              <EnterPost/>
+            </PostRow>
+            <PersonPost>
+              <Picture/>
+              <Name/>
+              <Post/>
+            </PersonPost>
+          </FeedBox>
+          <AddBox>
+            <Description/>
+            <AddPerson>
+              <Picture/>
+              <Name/>
+              <AddButton/>
+            </AddPerson>
+          </AddBox>
+        </div>
+        <Background/>
       //<TicketList/>
     </div>
   );
